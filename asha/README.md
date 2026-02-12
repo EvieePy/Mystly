f31bf820ca7d0bb66482e41d3a523250b3a9a6c5
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-02-12 17:18:52 +1000

    Add basic layout and fonts

--- asha/README.md
+++ asha/README.md
@@ -15,7 +15,7 @@ To recreate this project with the same configuration:
 
 ```sh
 # recreate this project
-npx sv create --template minimal --types ts --add prettier eslint --install npm asha
+npx sv create --template minimal --types ts --add prettier eslint sveltekit-adapter="adapter:static" --install npm asha
 ```
 
 ## Developing
--- asha/src/routes/+layout.svelte
+++ asha/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import "./layout.css";
+	import "./fonts.css";
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -8,4 +10,9 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="layout">
+	<aside></aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
